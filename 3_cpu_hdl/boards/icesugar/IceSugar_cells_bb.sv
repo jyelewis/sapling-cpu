cd836db87063fe4041323a9e3e72e0d1caece751
Jye Lewis <jye@jyelewis.com>
2026-04-19 20:19:36 +1000

    SB_GB, github actions

---
+++ 3_cpu_hdl/boards/icesugar/IceSugar_cells_bb.sv
@@ -0,0 +1,11 @@
+// verilog_lint: waive-start module-filename
+
+// all available cells here:
+// ~/dev-thirdparty/oss-cad-suite/share/yosys/ice40/cells_sim.v
+
+(* blackbox *)
+module SB_GB (
+    input  USER_SIGNAL_TO_GLOBAL_BUFFER,
+    output GLOBAL_BUFFER_OUTPUT
+);
+endmodule
--- 3_cpu_hdl/boards/icesugar/top.sv
+++ 3_cpu_hdl/boards/icesugar/top.sv
@@ -9,6 +9,8 @@ module top
 
     output logic [7:0] ICESUGAR_PMOD2_LED
 );
+
+  logic generated_clk;
   logic clk;
 
   // iCESugar clock is 12 MHz - divide down to get a 1hz clock
@@ -16,7 +18,13 @@ module top
       .DIVISOR(12_000_000)
   ) clock_divider (
       .clk_in (ICESUGAR_CLK),
-      .clk_out(clk)
+      .clk_out(generated_clk),
+      .reset  (1'b0)
+  );
+
+  SB_GB u_gb_step (
+      .USER_SIGNAL_TO_GLOBAL_BUFFER(generated_clk),
+      .GLOBAL_BUFFER_OUTPUT        (clk)
   );
 
   color_t       color;
